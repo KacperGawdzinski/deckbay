af90efba1101652d01e45dbb862abc287df8f894
KacperGawdzinski <58347130+KacperGawdzinski@users.noreply.github.com>
2021-01-01 05:06:20 +0100

    game room info sending

--- public/javascripts/game_room.js
+++ public/javascripts/game_room.js
@@ -1,7 +1,15 @@
-const room_name = document.getElementById("room_name").textContent
-
 window.addEventListener('load', function() {
     var socket = io();
-    
-    socket.emit('join-new-room', { game: game_type, room: room_name} );
+    socket.on('connect', () => {
+        $.ajax({
+            type: "POST",
+            url: "/set-socket-id",
+            data: {socketid: socket.id},
+            success: function(data) {
+                let room_name = data.substr(data.indexOf('-')+1, data.length);
+                let game_type = data.substr(0, data.indexOf('-'))
+                socket.emit('join-new-room', { game: game_type, room: room_name });
+            }
+        })
+     });
 })
