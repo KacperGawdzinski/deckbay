ad7a7e61ea51c657543129c8a4cbbd1053292d55
KacperGawdzinski <58347130+KacperGawdzinski@users.noreply.github.com>
2021-02-16 04:04:35 +0100

    react slide animation

--- client/src/components/RoomList/NewRoom/NewRoom.js
+++ client/src/components/RoomList/NewRoom/NewRoom.js
@@ -1,6 +1,6 @@
 import './NewRoom.css'
 
-const NewRoom = () => {
+const NewRoom = ({ game }) => {
     return (
     <div className="new_room_div">
         <form id="new_room_form" className="new_room_form" autoComplete="off">
@@ -10,7 +10,26 @@ const NewRoom = () => {
 
             <label>Password</label>
             <input type="password" id="room_password" placeholder="(optional)"/>
-
+            {(game === 'chess' || game === 'checkers') &&
+            <div>
+                <div style={{display: "flex", justifyContent: "space-around", marginTop: "10px"}}>
+                    <div className="ck-button">
+                        <label>
+                            <input type="checkbox" id="black" value="1" hidden/><span>Black</span>
+                        </label>
+                    </div>
+                    <div className="ck-button">
+                        <label>
+                            <input type="checkbox" id="white" value="1" hidden/><span>White</span>
+                        </label>
+                    </div>
+                </div>
+                <div style={{display: "flex", justifyContent: "space-around", marginTop: "10px", marginBottom: "10px"}}>
+                    <input id="glength" type="number" placeholder="Game length" min="1" max="30" step="1"/>
+                    <input id="blength" type="number" placeholder="Move bonus" min="1" max="30" step="1"/>
+                </div>
+            </div>}
+            <input id="new_room_submit" type="submit" value="Create new room!"></input>
         </form>
     </div>
 )}
