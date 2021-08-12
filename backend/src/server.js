9a10751bc3d8f679ae81b3697f3a5f65f8ae087c
Author Name <gawdzinskikacper@gmail.com>
2021-08-13 00:05:41 +0200

    dockerfiles

--- backend/src/server.js
+++ backend/src/server.js
@@ -55,20 +55,23 @@ app.use(cookieParser(process.env.COOKIE_SECRET_KEY));
 app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.static('public'));
 
-app.use('/', account);
+console.log("xd");
+app.use('/', (req, res) => {
+    res.send("xd")
+});
 app.use('/charades', charades);
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-mongoose
-    .connect(DB, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-    })
-    .then(() => {
-        console.log('Database connected');
-    });
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+// mongoose
+//     .connect(DB, {
+//         useUnifiedTopology: true,
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//     })
+//     .then(() => {
+//         console.log('Database connected');
+//     });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
