ea74a1b62a606001cf1f250b995b50855b194697
Author Name <gawdzinskikacper@gmail.com>
2021-08-13 21:33:01 +0200

    theme for app

---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@material-ui/core/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#35383d",
+      light: "#5f6267",
+      dark: "#0f1217",
+      contrastText: "#ffffff",
+    },
+  },
+});
+
+export default theme;
