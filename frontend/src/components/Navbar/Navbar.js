6a95812e1300948aba7c8398b833895ca05d6b8f
Author Name <gawdzinskikacper@gmail.com>
2021-08-13 22:16:46 +0200

    appBar with logo and login && register buttons

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -5,6 +5,7 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import InputBase from "@material-ui/core/InputBase";
+import Button from "@material-ui/core/Button";
 import Badge from "@material-ui/core/Badge";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
@@ -16,68 +17,38 @@ import NotificationsIcon from "@material-ui/icons/Notifications";
 import MoreIcon from "@material-ui/icons/MoreVert";
 import logo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
+import theme from "../../theme";
 
 const useStyles = makeStyles((theme) => ({
-  grow: {
-    flexGrow: 1,
+  background: {
+    backgroundColor: theme.palette.primary.dark,
+    flex: 1,
   },
+  logo: {
+    width: "200px",
+    height: "auto",
+    [theme.breakpoints.down("sm")]: {
+      width: "150px",
+    },
+  },
+  leftGrow: {
+    flex: 1,
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
+  },
+  rightGrow: {
+    flex: 0.8,
+    [theme.breakpoints.down("sm")]: {
+      flex: 1,
+    },
+  },
+
   menuButton: {
     marginRight: theme.spacing(2),
   },
-  title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block",
-    },
-  },
-  search: {
-    position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto",
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   inputRoot: {
     color: "inherit",
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-  sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex",
-    },
-  },
-  sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none",
-    },
   },
 }));
 
@@ -133,22 +104,6 @@ export default function PrimarySearchAppBar() {
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
-      <MenuItem>
-        <IconButton aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="secondary">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton aria-label="show 11 new notifications" color="inherit">
-          <Badge badgeContent={11} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
       <MenuItem onClick={handleProfileMenuOpen}>
         <IconButton
           aria-label="account of current user"
@@ -164,128 +119,16 @@ export default function PrimarySearchAppBar() {
   );
 
   return (
-    <div className={classes.grow}>
-      <AppBar position="static">
-        <Toolbar>
-          <Link to="/">
-            <img src={logo} alt="Deckbay logo" />
-          </Link>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="open drawer"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography className={classes.title} variant="h6" noWrap>
-            Material-UI
-          </Typography>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              placeholder="Search…"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ "aria-label": "search" }}
-            />
-          </div>
-          <div className={classes.grow} />
-          <div className={classes.sectionDesktop}>
-            <IconButton aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton aria-label="show 17 new notifications" color="inherit">
-              <Badge badgeContent={17} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </div>
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </div>
+    <AppBar position="static" className={classes.background}>
+      <Toolbar>
+        <div className={classes.leftGrow} />
+        <Link to="/">
+          <img src={logo} alt="Deckbay logo" className={classes.logo} />
+        </Link>
+        <div className={classes.rightGrow} />
+        <Button color="inherit">Login</Button>
+        <Button color="inherit">Register</Button>
+      </Toolbar>
+    </AppBar>
   );
 }
-
-// const Navbar = () => {
-//   const [loginLabel, setLoginLabel] = useState("Sign in");
-//   const [showLoginModal, setShowLoginModal] = useState(false);
-//   const [showRegisterModal, setShowRegisterModal] = useState(false);
-//   const handleCloseLoginModal = () => setShowLoginModal(false);
-//   const handleShowLoginModal = () => setShowLoginModal(true);
-//   const handleCloseRegisterModal = () => setShowRegisterModal(false);
-//   const handleShowRegisterModal = () => setShowRegisterModal(true);
-
-//   const Logout = () => {
-//     axios.post("/logout");
-//     setLoginLabel("Sign in");
-//   };
-
-//   return (
-//     <div></div>
-//     // <div>
-//     //     <AccountModal
-//     //         type={'login'}
-//     //         show={showLoginModal}
-//     //         handleClose={handleCloseLoginModal}
-//     //         setLoginLabel={setLoginLabel}
-//     //     />
-//     //     <AccountModal type={'register'} show={showRegisterModal} handleClose={handleCloseRegisterModal} />
-//     //     <header className="header">
-//     //         <Link to="/">
-//     //             <img src={logo} alt="Deckbay logo" />
-//     //         </Link>
-//     //         {loginLabel === 'Sign in' && (
-//     //             <div className="header_button_container">
-//     //                 <button className="header_button" onClick={handleShowLoginModal}>
-//     //                     Sign in
-//     //                 </button>
-//     //                 <button className="header_button" onClick={handleShowRegisterModal}>
-//     //                     Sign up
-//     //                 </button>
-//     //             </div>
-//     //         )}
-//     //         {loginLabel != 'Sign in' && (
-//     //             <div className="header_button_container">
-//     //                 <button className="header_button">{loginLabel}</button>
-//     //                 <button className="header_button" onClick={Logout}>
-//     //                     Logout
-//     //                 </button>
-//     //             </div>
-//     //         )}
-//     //         <span className="menu" />
-//     //     </header>
-//     // </div>
-//   );
-// };
-
-// export default Navbar;
