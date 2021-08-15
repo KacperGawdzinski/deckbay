744bce626bc398fc6f7443136640e04d7c07f4b7
KacperGawdzinski <gawdzinskikacper@gmail.com>
2021-08-16 00:15:26 +0200

    navbar change for userMenu after logging in

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,21 +1,19 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
-import LoginModal from "../Modals/LoginModal";
-import logo from "../../assets/images/logo.png";
-import { Link } from "react-router-dom";
-import { Typography } from "@material-ui/core";
-import RegisterModal from "../Modals/RegisterModal";
-import { IconButton } from "@material-ui/core";
-import MenuIcon from "@material-ui/icons/Menu";
+import { Typography } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/images/logo.png';
+import LoginModal from '../Modals/LoginModal';
+import RegisterModal from '../Modals/RegisterModal';
 
 export default function Navbar() {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
   const [openLoginModal, toggleLoginModal] = React.useState(false);
   const [openRegisterModal, toggleRegisterModal] = React.useState(false);
 
@@ -27,52 +25,26 @@ export default function Navbar() {
     toggleRegisterModal(true);
   };
 
-  const isMenuOpen = Boolean(anchorEl);
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
-
-  const menuId = "primary-search-account-menu";
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
-
   return (
     <AppBar position="static" className={classes.background}>
       <Toolbar>
-        <div className={classes.logoContainer}>
-          <Link to="/">
-            <img src={logo} alt="Deckbay logo" className={classes.logo} />
-          </Link>
-        </div>
+        <Link to="/">
+          <img src={logo} alt="Deckbay logo" className={classes.logo} />
+        </Link>
+        <div style={{ flexGrow: 1 }} />
         <div className={classes.buttonContainer}>
           <Button color="inherit" onClick={handleClickOpenLogin}>
             <Typography>Login</Typography>
           </Button>
-
           <Button color="inherit" onClick={handleClickOpenRegister}>
             <Typography>Register</Typography>
           </Button>
         </div>
         <IconButton
           edge="start"
-          className={classes.menuButton}
           color="inherit"
           aria-label="menu"
-          className={classes.hamburgerMenu}
-        >
+          className={classes.hamburgerMenu}>
           <MenuIcon />
         </IconButton>
         <LoginModal open={openLoginModal} setOpen={toggleLoginModal} />
@@ -88,45 +60,29 @@ const useStyles = makeStyles((theme) => ({
   },
 
   buttonContainer: {
-    display: "flex",
-    position: "absolute",
-    right: "20px",
-    [theme.breakpoints.down("xs")]: {
-      right: "0px",
-      display: "none",
-    },
-  },
-
-  logoContainer: {
-    marginLeft: "auto",
-    marginRight: "auto",
-    [theme.breakpoints.down("sm")]: {
-      marginLeft: 0,
+    [theme.breakpoints.down('xs')]: {
+      display: 'none',
     },
   },
 
   logo: {
-    width: "200px",
-    height: "auto",
-    left: "100px",
-    [theme.breakpoints.down("xs")]: {
-      width: "150px",
+    width: '215px',
+    height: 'auto',
+    paddingTop: 3,
+    [theme.breakpoints.down('xs')]: {
+      width: '150px',
     },
   },
 
   hamburgerMenu: {
-    display: "none",
-    [theme.breakpoints.down("xs")]: {
-      display: "flex",
-      marginLeft: "auto",
+    display: 'none',
+    [theme.breakpoints.down('xs')]: {
+      display: 'flex',
+      marginLeft: 'auto',
     },
   },
 
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-
   inputRoot: {
-    color: "inherit",
+    color: 'inherit',
   },
 }));
