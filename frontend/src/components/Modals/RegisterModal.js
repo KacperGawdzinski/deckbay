8ec08eb71b2145cfd12b4f8ef4fa5ffcdce8bef8
KacperGawdzinski <gawdzinskikacper@gmail.com>
2021-08-15 16:55:19 +0200

    circular progress on register

--- frontend/src/components/Modals/RegisterModal.js
+++ frontend/src/components/Modals/RegisterModal.js
@@ -1,3 +1,4 @@
+import { CircularProgress } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -7,26 +8,32 @@ import TextField from '@material-ui/core/TextField';
 import axios from 'axios';
 import React, { useState } from 'react';
 
+import { LOADING_STEPS } from '../../config';
+
 export default function RegisterModal(props) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
+  const [loadingStep, setLoadingStep] = useState(LOADING_STEPS.INPUT_DATA);
 
   const handleClose = () => {
     props.setOpen(false);
   };
 
   const handleRegister = async () => {
-    try {
-      await axios.post('http://localhost:5000/register', {
-        username: username,
-        password: password,
-        email: email,
-      });
-      handleClose();
-    } catch (err) {
-      console.log(err.response);
-    }
+    setLoadingStep(LOADING_STEPS.FETCHING_RESPONSE);
+    setTimeout(async () => {
+      try {
+        await axios.post('http://localhost:5000/register', {
+          username: username,
+          password: password,
+          email: email,
+        });
+        handleClose();
+      } catch (err) {
+        console.log(err.response);
+      }
+    }, 3000);
   };
   return (
     <Dialog
@@ -35,9 +42,13 @@ export default function RegisterModal(props) {
       aria-labelledby="form-dialog-title">
       <DialogTitle
         id="form-dialog-title"
-        style={{ alignSelf: 'center', paddingBottom: 0 }}>
+        style={{ marginLeft: 'auto', marginRight: 'auto', paddingBottom: 0 }}>
         Register
+        {loadingStep === LOADING_STEPS.FETCHING_RESPONSE && (
+          <CircularProgress style={{ position: 'absolute', right: 10 }} />
+        )}
       </DialogTitle>
+
       <DialogContent style={{ width: '400px' }}>
         <TextField
           autoFocus
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,7 @@
-export const GAME_LIST = ["Checkers", "Chess", "Charades"];
+export const GAME_LIST = ['Checkers', 'Chess', 'Charades'];
+export const LOADING_STEPS = {
+  INPUT_DATA: 1,
+  FETCHING_RESPONSE: 2,
+  POSITIVE_RESPONSE: 3,
+  NEGATIVE_RESPONSE: 4,
+};
